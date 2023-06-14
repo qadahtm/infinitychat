45c2b7530ba310826d8b6de739ff8af5c2a1e507
MouradMars <mars.mourad@gmail.com>
2023-06-14 13:34:04 +0300

    update logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -137,7 +137,7 @@ export default function Home() {
                     icon = (
                       <Image
                         key={index}
-                        src="/bot-image.png"
+                        src="/bot-logo.png"
                         alt="AI"
                         width="40"
                         height="40"
