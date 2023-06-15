9bbfc03738d9a4a9430426c891a60b6d48049c0e
MouradMars <mars.mourad@gmail.com>
2023-06-16 02:21:27 +0300

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -125,7 +125,7 @@ export default function Home() {
       <Layout>
         <div className="mx-auto flex flex-col gap-4">
           <h1 className="text-2xl font-bold leading-[1.1] tracking-tighter text-center">
-            InfinityChat: AI Powered mutli-channel solution to chat with your data.</>
+            InfinityChat: AI Powered mutli-channel solution to chat with your data.
           </h1>
           <main className={styles.main}>
             <div className={styles.cloud}>
