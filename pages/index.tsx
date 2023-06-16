b2866a01c74ea97315162f4e7734b691cfd3e6b5
Mohamed Essabir <mohamedessabir20@gmail.com>
2023-06-16 21:30:49 +0100

    telegram bot

--- pages/index.tsx
+++ pages/index.tsx
@@ -202,15 +202,15 @@ export default function Home() {
                               <div key={`messageSourceDocs-${index}`}>
                                 <AccordionItem value={`item-${index}`}>
                                   <AccordionTrigger>
-                                    <h3>Source {index + 1}</h3>
+                                    <h3 className='text-white'>Source {index + 1}</h3>
                                   </AccordionTrigger>
                                   <AccordionContent>
-                                    <ReactMarkdown linkTarget="_blank">
+                                    <ReactMarkdown linkTarget="_blank" className='text-white'>
                                       {doc.pageContent}
                                     </ReactMarkdown>
-                                    <p className="mt-2">
+                                    {/* <p className="mt-2">
                                       <b>Source:</b> {doc.metadata.source}
-                                    </p>
+                                    </p> */}
                                   </AccordionContent>
                                 </AccordionItem>
                               </div>
